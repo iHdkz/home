f9dd7bc2e697a08dd0332e69121cb03dcb8b0a1a
iHdkz <i.hidekazu@gmail.com>
2015-07-05 07:48:26 +0900

    add some aliases

--- .local/functions/aliases.sh
+++ .local/functions/aliases.sh
@@ -1,13 +1,37 @@
 case "${OSTYPE}" in
 	darwin*)
 		alias ls='ls -G'
-		alias ll='ls -lG'
-		alias la='ls -laG'
 		;;
 	linux*)
-		alias ls='ls --color'
-		alias ll='ls -l --color'
-		alias la='ls -la --color'
+		alias ls='ls -BF --color=auto --show-control-char'
+		;;
+	BSD*)
+		if [[ -x gls ]] ; then
+			alias ls="gls -BF --color=auto --show-control-char"
+		else
+			alias ls="/bin/ls -Fw"
+		fi
 		;;
 esac
+
+alias l.="ls .*"
+alias la="ls -a"
+alias ll="ls -hl"
+alias lla="ls -ahl"
+alias less=${PAGER:=less}
+alias man="w3mman"
+alias du='/bin/du -hk'
+alias mv='/bin/mv -iv'
+alias cp='/bin/cp -iv'
+alias rm='/bin/rm -iv'
+alias info='/usr/bin/info --vi-keys'
+alias bc='/usr/bin/bc -l'
+alias ln='/usr/bin/ln -i'
+unalias grep 2> /dev/null ; alias grep="grep --color=auto"
+unalias lftp 2> /dev/null ; alias lftp="lftp -e 'set bmk:save-passwords on && set cmd:prompt \[\e[34m\]\w\[\e[32m\]\ \>\>\[\e[0m\]\ '"
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
 
