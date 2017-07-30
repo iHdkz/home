ea20b854e602d99a6097afe6ad9fe67fff0861b3
iHdkz <i.hidekazu@gmail.com>
2017-07-30 02:31:28 +0000

    modified conf.sh

--- .local/functions/conf.sh
+++ .local/functions/conf.sh
@@ -1,15 +1,20 @@
-[[ $OSTYPE == darwin* ]] && __LS_OPT='-G'
-[[ $OSTYPE == linux*  ]] && __LS_OPT='-BF --color=auto --show-control-char'
-[[ $OSTYPE == *bsd*   ]] && __LS_OPT='-Fw'
+
+__OSNAME=$(uname)
+[ "${__OSNAME}" != "${__OSNAME%darwin*}" ] && __LS_OPT='-G'
+[ "${__OSNAME}" != "${__OSNAME%linux* }" ] && __LS_OPT='-BF --color=auto --show-control-char'
+[ "${__OSNAME}" != "${__OSNAME#*BSD}"    ] && __LS_OPT='-Fw'
+
 alias ls="\ls "$__LS_OPT
-[[ -x gls ]] && alias ls="gls -BF --color=auto --show-control-char"
+[ -x gls ] && alias ls="gls -BF --color=auto --show-control-char"
 
 alias l.="ls .*"
 alias la="ls -a"
 alias ll="ls -hl"
 alias lla="ls -ahl"
-alias less=${PAGER:=w3m}
-alias man="w3mman"
+[ ! -z $(which vim) ]    && alias vi="\vim"
+[ ! -z $(which w3mman) ] && alias man="\w3mman"
+[ ! -z $(which w3m) ]    && PAGER=w3m
+alias less=${PAGER:=less}
 alias mv='\mv -iv'
 alias cp='\cp -iv'
 alias rm='\rm -iv'
@@ -21,7 +26,7 @@ alias lftp="\lftp -e 'set bmk:save-passwords on && set cmd:prompt \[\e[34m\]\w\[
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-if [[ $ZSH_NAME == zsh ]]; then
+if [ ! -z "$ZSH_NAME" ]; then
 	alias -s gp="gnuplot"
 	alias -s gnu="gnuplot"
 fi
