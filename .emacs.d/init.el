555a0b813cecca309786ee6a7dc8e2626b60eb66
iHdkz <i.hidekazu@gmail.com>
2020-05-31 21:22:34 +0900

    remove .emacs*

--- .emacs.d/init.el
+++
@@ -1,8 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
-
-
-(require 'init-loader)
-(init-loader-load "~/.emacs.d/inits")
--- .emacs.d/inits/30-agda-mode.el
+++
@@ -1,6 +0,0 @@
-;; Agda-mode setting
-
-(load-file (let ((coding-system-for-read 'utf-8))
-                 (shell-command-to-string "agda-mode locate")))
-
-(__DEBUG (shell-command-to-string "agda-mode locate"))
